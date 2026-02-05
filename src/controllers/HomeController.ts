b25fc32b5d10e7262376f370da12a6b67ed702a5
Alejandra Suarez <mayerlysuarez1508@gmail.com>
2026-02-05 12:24:56 -0500

    Mostrar un libro

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express';
-import { books } from '../data/Books.js'; 
+import { books } from '../data/Books.js';
+import { Book } from '../models/Book.js'; 
 
 export class HomeController {
     static index(req: Request, res: Response): void {
@@ -24,5 +25,10 @@ export class HomeController {
         const viewData: any = {};
         viewData["books"] = books;
         res.render('home/books', viewData); 
+    }
+
+    static show(req: any, res: any){
+        const book = Book.findById(books, parseInt(req.params.id));
+        res.render('home/show', { book: book })
     }
 }
